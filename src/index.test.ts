8f0649e59699001844a090d7471c099a4ab4a511
davzlum <davzlum@gmail.com>
2022-11-21 18:42:57 +0100

    Add time out and create transaction file

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,32 +1,43 @@
-import myBankAccount from "./index";
+import Account from "./index";
 
 describe("Given a bank application", () => {
-  let bankStatement: jest.Spied<typeof console.table>;
+  let transactions: jest.Spied<typeof console.table>;
   let rejectionMessage: jest.Spied<typeof console.log>;
-  const bankAccountClass = myBankAccount();
-  let myAccount = new bankAccountClass();
+  let sessionExpiredMessage: jest.Spied<typeof console.log>;
+  let myAccount: any;
   let actualDate: string;
+  jest.useFakeTimers();
 
   beforeEach(() => {
-    bankStatement = jest.spyOn(console, "table");
+    myAccount = new Account();
+    transactions = jest.spyOn(console, "table");
     rejectionMessage = jest.spyOn(console, "log");
     actualDate = new Date().toLocaleDateString();
   });
 
-  test("When a client deposit a quantity of money into an account, then the quantity should increase", () => {
-    myAccount.deposit(500);
-    myAccount.deposit(1000);
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+  });
 
-    expect(bankStatement).toHaveBeenCalledWith([
-      { date: actualDate, amount: 1000, balance: 1500 },
-      { date: actualDate, amount: 500, balance: 500 },
-    ]);
+  test("When a client deposit a quantity of money into an account, then the quantity should increase", () => {
+    expect(myAccount.deposit(500)).toBe(500);
   });
 
   test("When a client withdraw a quantity of money into an account, then the quantity should decrease", () => {
+    myAccount.deposit(500);
+
+    expect(myAccount.withdraw(300)).toBe(200);
+  });
+
+  test("When a client a client wants to get an extract of the transactions, then should receive a console log with the information", () => {
+    myAccount.deposit(500);
+    myAccount.deposit(1000);
     myAccount.withdraw(300);
 
-    expect(bankStatement).toHaveBeenCalledWith([
+    myAccount.printTransactions();
+
+    expect(transactions).toHaveBeenCalledWith([
       { date: actualDate, amount: -300, balance: 1200 },
       { date: actualDate, amount: 1000, balance: 1500 },
       { date: actualDate, amount: 500, balance: 500 },
@@ -34,10 +45,22 @@ describe("Given a bank application", () => {
   });
 
   test("When a client withdraw a quantity of money greater than the amount of money in the account, then a console log rejecting the operation should appear", () => {
+    myAccount.deposit(500);
     myAccount.withdraw(1300);
 
     expect(rejectionMessage).toHaveBeenCalledWith(
       "Permission denied, you dont have enought money into your account"
+    );
+  });
+
+  test("When a client gets the session opened for 30 sec, then the session should expire and a console log should appear", () => {
+    jest.spyOn(global, "setTimeout");
+    sessionExpiredMessage = jest.spyOn(console, "log");
+    jest.advanceTimersByTime(31000);
+
+    expect(sessionExpiredMessage).toBeCalled();
+    expect(sessionExpiredMessage).toHaveBeenLastCalledWith(
+      "Your session has expired"
     );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,66 @@
-interface BankMovementDetail {
+const fs = require("fs-extra");
+
+interface Transaction {
   date: string;
   amount: number;
   balance: number;
 }
 
-type BankMovementDetails = BankMovementDetail[];
+type Transactions = Transaction[];
 
-export default function myBankAccount() {
-  class Account {
-    #amount: number = 0;
-    #money: number = 0;
-    #date: string = new Date().toLocaleDateString();
-    #bankMovementDetail: BankMovementDetail = {
+export default class Account {
+  #amount: number = 0;
+  #money: number = 0;
+  #date: string = new Date().toLocaleDateString();
+  #transaction: Transaction = {
+    date: this.#date,
+    amount: this.#amount,
+    balance: this.#money,
+  };
+  #transactions: Transactions = [];
+
+  constructor() {
+    this.shutdownSession();
+  }
+
+  public deposit(amount: number) {
+    this.#amount = amount;
+    this.#money += amount;
+    this.saveTransaction();
+    return this.#money;
+  }
+
+  public withdraw(amount: number) {
+    if (amount > this.#money)
+      return console.log(
+        "Permission denied, you dont have enought money into your account"
+      );
+    this.#amount = -amount;
+    this.#money -= amount;
+    this.saveTransaction();
+    return this.#money;
+  }
+
+  private saveTransaction() {
+    this.#transaction = {
       date: this.#date,
       amount: this.#amount,
       balance: this.#money,
     };
-    #bankMovementDetails: BankMovementDetails = [];
-
-    public deposit(amount: number) {
-      this.#amount = amount;
-      this.#money += amount;
-      this.printStatement();
-      return this.#money;
-    }
-    public withdraw(amount: number) {
-      if (amount > this.#money)
-        return console.log(
-          "Permission denied, you dont have enought money into your account"
-        );
-      this.#amount = -amount;
-      this.#money -= amount;
-      this.printStatement();
-      return this.#money;
-    }
-    public printStatement() {
-      this.#bankMovementDetail = {
-        date: this.#date,
-        amount: this.#amount,
-        balance: this.#money,
-      };
-      console.log(this.#bankMovementDetail);
-      this.#bankMovementDetails = [
-        this.#bankMovementDetail,
-        ...this.#bankMovementDetails,
-      ];
-      console.table(this.#bankMovementDetails);
-    }
+    this.#transactions = [this.#transaction, ...this.#transactions];
+    fs.writeJsonSync("./transactions.json", {
+      transactions: this.#transactions,
+    });
   }
 
-  return Account;
+  public printTransactions() {
+    console.table(this.#transactions);
+  }
+
+  private shutdownSession() {
+    console.log("Ready....go!");
+    setTimeout(() => {
+      console.log("Your session has expired");
+    }, 30000);
+  }
 }
